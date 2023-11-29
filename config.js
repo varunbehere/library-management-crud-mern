2f1cb1d5513c66eb240256c7503d5955e9d5b33f
varunbehere <varunbehere@gmail.com>
2023-11-29 12:22:04 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+export const PORT = 5555;
+
+export const mongoDBURL = process.env.mongoDBURL;
---
+++ index.js
@@ -0,0 +1,40 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Book Shop");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to db");
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
